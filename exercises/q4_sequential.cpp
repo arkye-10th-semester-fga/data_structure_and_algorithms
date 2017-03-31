c697792225b6c1d885cd2794ee1fb9b4b6aaa661
Jonathan Moraes <arkyebr@gmail.com>
2017-03-30 23:27:16 -0300

    Finish Exercise 1 - Question 4

--- exercises/q4_sequential.cpp
+++ exercises/q4_sequential.cpp
@@ -78,15 +78,19 @@ int main(int argc, char* argv[])
  */
 void create_primary_index_table(vector<long long> &primary_indexes, vector<long long> &sorted_elements)
 {
+	// Start with floor(n/10) elements
 	int primary_index_size = sorted_elements.size()/PRIMARY_INDEX_RATIO;
 
+	// Insert the first element
 	primary_indexes.push_back(sorted_elements[0]);
 
+	// Insert elements with 10*(n-1) indexes
 	for(int i = 1; i <= primary_index_size; ++i)
 	{
 		primary_indexes.push_back(sorted_elements[(i*PRIMARY_INDEX_RATIO)-1]);
 	}
 
+	// If not divisible by 10, insert the last element
 	if(sorted_elements.size() % PRIMARY_INDEX_RATIO != 0)
 	{
 		primary_indexes.push_back(sorted_elements[sorted_elements.size()-1]);
@@ -98,20 +102,48 @@ void create_primary_index_table(vector<long long> &primary_indexes, vector<long 
  */
 int find_primary_index(vector<long long> &primary_indexes, long long search_value)
 {
-	int index;
+	int index = 0;
 	const int SIZE_OF_PRIMARY_INDEXES = primary_indexes.size();
-
 	bool search_value_is_lesser_or_equals_element = false;
-	for(index = 0; !search_value_is_lesser_or_equals_element &&
-		index < SIZE_OF_PRIMARY_INDEXES; ++index)
+
+	// Search the minimum element greater or equals than the search value
+	while(!search_value_is_lesser_or_equals_element && index < SIZE_OF_PRIMARY_INDEXES)
 	{
 		if(search_value <= primary_indexes[index])
 		{
 			search_value_is_lesser_or_equals_element = true;
 		}
+		else
+		{
+			++index;
+		}
 	}
 
-	return (index-1)*PRIMARY_INDEX_RATIO-1;
+	// Correct the index
+	if(index > 1)
+	{
+		if(index < SIZE_OF_PRIMARY_INDEXES-1)
+		{
+			// Primary index is between 2 ~ n
+			// The first -1 is to get the previous index
+			// The second -1 is to correct the index (which starts at 0)
+			index = (index-1)*PRIMARY_INDEX_RATIO-1;
+		}
+		else
+		{
+			// Primary index is the last index
+			// The -2 is to get the previous index with index correction (which last is SIZE-1)
+			// The -1 is to correct the index (which starts at 0)
+			index = (SIZE_OF_PRIMARY_INDEXES-2)*PRIMARY_INDEX_RATIO-1;
+		}
+	}
+	else
+	{
+		// Primary index is 0 or 1, index must be the first
+		index = 0;
+	}
+
+	return index;
 }
 
 
@@ -123,18 +155,22 @@ int sequential_search(vector<long long> &sorted_elements, long long search_value
 {
 	int index;
 	const int SIZE_OF_SORTED_ELEMENTS = sorted_elements.size();
+
+	// Search the value with start index defined by the primary index table
 	for(index = initial_index; index < SIZE_OF_SORTED_ELEMENTS; ++index)
 	{
 		if(sorted_elements[index] == search_value)
 		{
+			// The value was found
 			return index;
 		}
 
 		if(sorted_elements[index] > search_value)
 		{
+			// The element is bigger than the value, so the value could not be found
 			return NOT_FOUND;
 		}
 	}
-
+	// The element was not found (uncanny behavior)
 	return NOT_FOUND;
 }
